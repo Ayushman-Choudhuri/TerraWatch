4065286f808c27ce9cbf798b67afbc5ff3557a79
tobi <mail.tobias.leibrock@gmail.com>
2024-04-27 19:11:50 +0200

    add new requirements and gitignore

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,3 +1,7 @@
+import numpy as np
+from PIL import Image
+
+
 def center_crop(img, dim):
     """Returns center cropped image
     Args:
@@ -7,19 +11,16 @@ def center_crop(img, dim):
     width, height = img.shape[1], img.shape[0]
 
     # process crop width and height for max available dimension
-    crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
-    crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0]
-    mid_x, mid_y = int(width/2), int(height/2)
-    cw2, ch2 = int(crop_width/2), int(crop_height/2)
-    crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
+    crop_width = dim[0] if dim[0] < img.shape[1] else img.shape[1]
+    crop_height = dim[1] if dim[1] < img.shape[0] else img.shape[0]
+    mid_x, mid_y = int(width / 2), int(height / 2)
+    cw2, ch2 = int(crop_width / 2), int(crop_height / 2)
+    crop_img = img[mid_y - ch2 : mid_y + ch2, mid_x - cw2 : mid_x + cw2]
     return crop_img
 
+
 def get_png(arr):
-    color_map = {
-        0: (255, 0, 0),  # Red
-        1: (0, 255, 0),  # Green
-        2: (0, 0, 255)  # Blue
-    }
+    color_map = {0: (255, 0, 0), 1: (0, 255, 0), 2: (0, 0, 255)}  # Red  # Green  # Blue
 
     data = arr
     # Create an empty array for the RGB values
@@ -35,6 +36,6 @@ def get_png(arr):
             rgb_array[i, j] = color_map[label_index]
 
     # Convert the numpy array to a Pillow Image
-    image = Image.fromarray(rgb_array, 'RGB')
+    image = Image.fromarray(rgb_array, "RGB")
 
     return image
