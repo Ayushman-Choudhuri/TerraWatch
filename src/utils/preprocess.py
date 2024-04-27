7358029bca2f946a22548a657736ffed2d2bc824
Ayushman-Choudhuri <ayushc205@gmail.com>
2024-04-27 18:20:15 +0200

    feat: preprocess updated with get_png

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -13,3 +13,28 @@ def center_crop(img, dim):
     cw2, ch2 = int(crop_width/2), int(crop_height/2)
     crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
     return crop_img
+
+def get_png(arr):
+    color_map = {
+        0: (255, 0, 0),  # Red
+        1: (0, 255, 0),  # Green
+        2: (0, 0, 255)  # Blue
+    }
+
+    data = arr
+    # Create an empty array for the RGB values
+    height, width, _ = data.shape
+    rgb_array = np.zeros((height, width, 3), dtype=np.uint8)
+
+    # Fill the rgb_array with colors based on the highest probability label
+    for i in range(height):
+        for j in range(width):
+            # Get the index of the highest probability
+            label_index = np.argmax(data[i, j])
+            # Map this label index to a color
+            rgb_array[i, j] = color_map[label_index]
+
+    # Convert the numpy array to a Pillow Image
+    image = Image.fromarray(rgb_array, 'RGB')
+
+    return image
