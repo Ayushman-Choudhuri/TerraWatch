9ff3eee5b53dfd817c824b3796d2528ec5d87d6c
4kshay18 <33688041+4kshay18@users.noreply.github.com>
2024-04-27 15:37:16 +0200

    model.py added

---
+++ model.py
@@ -0,0 +1,191 @@
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.compat.v1.Session(config=config)
+
+from tensorflow.keras import backend as K
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation,\
+                                    MaxPool2D, UpSampling2D, concatenate,\
+                                    Input, Conv2DTranspose, MaxPooling2D,\
+                                    Dropout, BatchNormalization
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import CSVLogger, ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# import tensorflow.keras as K
+
+# config = tf.compat.v1.ConfigProto()
+# config.gpu_options.allow_growth = True
+# sess = tf.compat.v1.Session(config=config)
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+import numpy as np
+# from numba import cuda
+import datetime
+import copy
+import PIL
+import sys
+import random
+import os
+from matplotlib import colors
+
+from tensorflow.keras import backend as K
+
+# random.seed(42)
+# tf.random.set_seed(42)
+# np.random.seed(42)
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import cuda
+import datetime
+
+def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True, sublayers=2):
+    '''In case batchnorm=False "if" statement will be skipped and in amount of "sublayers" convolutional layers will be created.'''
+    for idx in range(sublayers):
+        conv = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size),
+                   kernel_initializer="he_normal", padding="same")(input_tensor if idx==0 else conv)
+        if batchnorm:
+            normalized = BatchNormalization()(conv)
+        conv = Activation("relu")(conv) # normalized to conv
+    return conv
+
+def conv2d_transpose_block(input_tensor, concatenate_tensor, n_filters, kernel_size=3, strides=2, transpose=False):
+    if transpose:
+        conv = Conv2DTranspose(n_filters, (kernel_size, kernel_size),
+                               strides=(strides, strides), padding='same')(input_tensor)
+    else:
+        conv = Conv2D(n_filters, (kernel_size, kernel_size), activation = 'relu', padding = 'same',
+                      kernel_initializer = 'he_normal')(UpSampling2D(size=(kernel_size, kernel_size))(input_tensor))
+    conv = Activation("relu")(conv)
+    concatenation = concatenate([conv, concatenate_tensor])
+    return concatenation
+
+def build_unet(input_shape=(512, 512, 3), filters=[16, 32, 64, 128, 256], batchnorm=True, transpose=False, dropout_flag=False):  # dropout_rate=0.2
+
+    conv_dict = dict()
+    inputs = Input(input_shape)
+    dropout_rate = 0.5
+
+    for idx, n_filters in enumerate(filters[:-1]):
+        conv = conv2d_block(inputs if n_filters==filters[0] else max_pool, # Change max_pool to dropout
+                            n_filters=n_filters, kernel_size=3,
+                            batchnorm=batchnorm)
+        max_pool = MaxPooling2D((2, 2))(conv)
+        # # Commented dropout
+        # if idx > 1 and dropout_flag:
+        #     print('[INFO] Dropout down')
+        #     dropout = Dropout(dropout_rate)(max_pool)
+        # else:
+        #     print('[INFO] Dropout down skip')
+        #     dropout = max_pool
+        # print(f'[INFO] Conv block {idx} created.')
+        # # Save layer
+        conv_dict[f"conv2d_{idx+1}"] = conv
+
+    # Change max_pool to dropout
+    conv_middle = conv2d_block(max_pool, n_filters=filters[-1], kernel_size=3, batchnorm=batchnorm)
+
+    for idx, n_filters in enumerate(reversed(filters[:-1])):
+        concatenation = conv2d_transpose_block(conv_middle if idx==0 else conv,
+                                               conv_dict[f"conv2d_{len(conv_dict) - idx}"],
+                                               n_filters, kernel_size=2, strides=2, transpose=transpose) # kernel_size=2, like in previous
+        # Commented Dropout
+#         if idx < len(filters) - 3 and dropout_flag:
+#             print('[INFO] Dropout up')
+#             dropout = Dropout(dropout_rate)(concatenation)
+#         else:
+#             print('[INFO] Dropout up skip')
+#             dropout = concatenation
+        conv = conv2d_block(concatenation, n_filters=n_filters, kernel_size=3, # Change concatenation to Dropout
+#                             batchnorm = batchnorm if idx not in [len(conv_dict), len(conv_dict) - 1] else False)
+                              batchnorm = batchnorm)
+        print(f'[INFO] UpConv block {idx} created.')
+    outputs = Conv2D(3, (1, 1), activation='softmax')(conv)
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
+def make_unet_experimental():
+  input_size = (512, 512,3)
+  inputs = Input(input_size)
+
+  conv1 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+  conv1 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+  conv2 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+  conv2 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+  up2_1  = Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv2))
+  pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+  conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+  conv3 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+  up3_1  = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv3))
+  up3_2  = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up3_1))
+  pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+  conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+  conv4 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+  up4_1  = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv4))
+  up4_2  = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up4_1))
+  up4_3  = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up4_2))
+  pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+  conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+  conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+  up5_1 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(conv5))
+  up5_2 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up5_1))
+  up5_3 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up5_2))
+  up5_4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(up5_3))
+
+  up6 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv5))
+  merge6 = concatenate([conv4, up6, up5_1], axis = 3)
+  conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+  conv6 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+  up7 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+  merge7 = concatenate([conv3, up7, up5_2, up4_1], axis = 3)
+  conv7 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+  conv7 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+  up8 = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+  merge8 = concatenate([conv2, up8, up5_3, up4_2, up3_1], axis = 3)
+  conv8 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+  conv8 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+  up9 = Conv2D(16, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+  merge9 = concatenate([conv1, up9, up5_4, up4_3, up3_2, up2_1], axis = 3)
+  conv9 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+  conv9 = Conv2D(16, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+
+  conv10 = Conv2D(3, 1, activation = 'softmax')(conv9)
+
+  model = Model(inputs = inputs, outputs = conv10)
+  return model
+
+final_filters = 2048
+model10 = build_unet(input_shape=(1024, 1024, 3),
+                     filters=[2 ** i for i in range(5, int(np.log2(final_filters) + 1))], # Amount of filters in U-Net arch.
+                     batchnorm=False, transpose=False, dropout_flag=False)
+
+path_to_load = "C:/Users/endez/Downloads/U6_E_1201-F1_0.7134-IOU_0.6555.h5"
+model10.load_weights(path_to_load)
+
+from PIL import Image
+test_4 = Image.open("C:/Users/endez/Documents/TUMAIMakeathon/map-screenshot-3.png")
+image = np.asarray(test_4)
+image = image[:1024,:1024,:3]
+image = image[np.newaxis, ...]
+# plt.imshow(image[0])
+# plt.show()
+
+prediction = model10.predict(image)
+
+prediction_class1 = np.copy(prediction[..., 0]) # Forest
+prediction_class2 = np.copy(prediction[..., 1]) # Deforest
+prediction[..., 0] = prediction_class2 # RED - Deforest
+prediction[..., 1] = prediction_class1 # GREEN - Forest
+plt.imshow(prediction[0])
+plt.show()
